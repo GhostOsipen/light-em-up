30f4373479d4c9a48027a9d428d8e78c7ee6f2d6
GhostOs <gomax94@gmail.com>
2021-05-14 18:56:32 +0300

    opencv start 14.05.21

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi")
+import cv2 as cv
+import numpy
+
+#read image
+img = cv.imread('data/1.jpg')
+
+def rescaleFrame(frame, scale=0.25):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+resized_image = rescaleFrame(img)
+cv.imshow('Cat', resized_image)
+
+cv.waitKey(0)
